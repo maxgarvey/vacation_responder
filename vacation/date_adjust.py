04f3d1a090bac15eef74a0ae94d22f2e877eca11
Max Garvey <maxgarvey@gmail.com>
2012-05-18 12:31:44 -0700

    moved shell files. date adjust uses datetime lib

--- vacation/date_adjust.py
+++ vacation/date_adjust.py
@@ -3,13 +3,11 @@ import datetime
 def plus_one(date_str):
     '''this method takes a date as a string in format "YYYY-MM-DD".
         it just takes one param, a string of the date'''
-    date_elements = date_str.split('-')
+    
+    date_elements = date_str.split('-') #split the date to subsections
+    #get a datetime object based on input string
     init_date = datetime.date(int(date_elements[0]),int(date_elements[1]),int(date_elements[2]))
-    try:
-        #try incrementing the day
-        adjusted_date = init_date.replace(day=init_date.day+1)
-    except Exception, err:
-        #if the date after incrementing the day falls in the next month, it will throw an
-        #exception, so then we just set the month to the next month, and the day to the first
-        adjusted_date = init_date.replace(day=1,month=init_date.month+1)
+    #use the datetime timedelta to logically add a day
+    adjusted_date = init_date + datetime.timedelta(days=1)
+    #join and return
     return '-'.join([str(adjusted_date.year),str(adjusted_date.month),str(adjusted_date.day)])
